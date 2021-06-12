7a8e81b7746829026ae46c790453b786f7a9e015
chienhowh <63964025+chienhowh@users.noreply.github.com>
2021-06-12 20:46:20 +0800

    svg fix

--- src/app/shared/rate-icon/rate-icon.component.ts
+++ src/app/shared/rate-icon/rate-icon.component.ts
@@ -6,11 +6,17 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./rate-icon.component.scss']
 })
 export class RateIconComponent implements OnInit {
+  // 分數
   @Input() rate: string;
+
+  // 儀表板長度
+  circleRate:number;
+  // 儀表板顏色
   circleColor: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.circleRate = +this.rate;
     if (+this.rate >= 7) { this.circleColor = 'green'; }
     else if (+this.rate >= 4) { this.circleColor = 'orange'; }
     else { this.circleColor = 'red'; }
