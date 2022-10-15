231c102f7955b32148b74959034884c2071e9e34
chienhowh <chienhowh@gmail.com>
2022-10-15 22:51:58 +0800

    feat: add prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  DEFAULT_IP: 'https://api.themoviedb.org/3',
+  DB_IP: 'https://movieback.duckdns.org',
+  FB_IP: 'https://react-meetup-aff59-default-rtdb.asia-southeast1.firebasedatabase.app',
+  GAPI_CLIENT_ID: '564087547062-3r0maars7kgpun3fonkjjpv616e88n95.apps.googleusercontent.com',
+  firebase: {
+    apiKey: 'AIzaSyDfXBFy3P1IPBncApDTKUBLHk6B9tiQwVQ',
+    authDomain: 'movieback-25655.firebaseapp.com',
+    databaseURL: 'https://movieback-25655-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'movieback-25655',
+    storageBucket: 'movieback-25655.appspot.com',
+    messagingSenderId: '703003658373',
+    appId: '1:703003658373:web:27acaf36f5d8a1082481ec',
+    measurementId: 'G-05BMZ9HPWF'
+  }
+
 };
